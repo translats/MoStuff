a6fbc188fde62ca636f2e0906fb62608cdeb8db5
JasperChaseTOQ <103849596+JasperChaseTOQ@users.noreply.github.com>
2022-06-17 18:02:50 -0400

    prep

--- src/main/java/me/jasperchasetoq/mostuff/MoStuffItems.java
+++ src/main/java/me/jasperchasetoq/mostuff/MoStuffItems.java
@@ -1,4 +1,18 @@
 package me.jasperchasetoq.mostuff;
 
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.groups.NestedItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.groups.SubItemGroup;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+
 public class MoStuffItems {
+    public static final NestedItemGroup MO_GENERAL = new NestedItemGroup(new NamespacedKey(MoStuff.getInstance(), "MO_GENERAL"), new CustomItemStack(Material.CHEST, "&bMoStuff"));
+    public static final SubItemGroup MO_WANDS = new SubItemGroup(new NamespacedKey(MoStuff.getInstance(), "MO_WANDS"), MO_GENERAL, new CustomItemStack(Material.BLAZE_ROD, "&bWands"));
+    public static final SubItemGroup MO_MISC = new SubItemGroup(new NamespacedKey(MoStuff.getInstance(), "MO_MISC"), MO_GENERAL, new CustomItemStack(Material.CHEST, "&bMisc"));
+    public static final SubItemGroup MO_INFO = new SubItemGroup(new NamespacedKey(MoStuff.getInstance(), "MO_INFO"), MO_GENERAL, new CustomItemStack(Material.BOOK, "&bAddon Info"));
+
+    public static final ItemGroup MoStuffGeneral = new ItemGroup(new NamespacedKey(MoStuff.getInstance(), "MoStuff"),
+            new CustomItemStack(Material.PISTON, "&bMoStuff"));
 }
