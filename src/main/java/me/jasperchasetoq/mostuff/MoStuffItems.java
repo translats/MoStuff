30630bc422c19e4a5c1e3b051e7ed54907aef6f7
haiman <83174104+haiman233@users.noreply.github.com>
2022-07-24 20:58:16 +0800

    汉化

--- src/main/java/me/jasperchasetoq/mostuff/MoStuffItems.java
+++ src/main/java/me/jasperchasetoq/mostuff/MoStuffItems.java
@@ -9,7 +9,7 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 
 public class MoStuffItems {
-    public static final NestedItemGroup MO_GENERAL = new NestedItemGroup(new NamespacedKey(MoStuff.getInstance(), "MO_GENERAL"), new CustomItemStack(Material.CHEST, "&bMoStuff"));
+    public static final NestedItemGroup MO_GENERAL = new NestedItemGroup(new NamespacedKey(MoStuff.getInstance(), "MO_GENERAL"), new CustomItemStack(Material.CHEST, "&b魔杖工艺"));
     public static final SubItemGroup MO_MACHINES = new SubItemGroup(new NamespacedKey(MoStuff.getInstance(), "MO_MACHINES"), MO_GENERAL, new CustomItemStack(Material.FURNACE, "&b机器"));
     public static final SubItemGroup MO_WANDS = new SubItemGroup(new NamespacedKey(MoStuff.getInstance(), "MO_WANDS"), MO_GENERAL, new CustomItemStack(Material.BLAZE_ROD, "&b魔杖"));
     public static final SubItemGroup MO_MISC = new SubItemGroup(new NamespacedKey(MoStuff.getInstance(), "MO_MISC"), MO_GENERAL, new CustomItemStack(Material.CHEST, "&b杂项"));
@@ -17,11 +17,11 @@ public class MoStuffItems {
     public static final SubItemGroup MO_INFO = new SubItemGroup(new NamespacedKey(MoStuff.getInstance(), "MO_INFO"), MO_GENERAL, new CustomItemStack(Material.BOOK, "&b插件信息"));
 
     public static final ItemGroup MoStuffGeneral = new ItemGroup(new NamespacedKey(MoStuff.getInstance(), "MoStuff"),
-            new CustomItemStack(Material.PISTON, "&bMoStuff"));
+            new CustomItemStack(Material.PISTON, "&b魔杖工艺"));
 
     //Wands
-    public static final SlimefunItemStack MO_TRANSMUTATION_WAND = new SlimefunItemStack("MO_TRANSMUTATION_WAND", Material.BLAZE_ROD, "&d蜕变魔杖", "", "&f将某些物品转化为下一种形式", "");
+    public static final SlimefunItemStack MO_TRANSMUTATION_WAND = new SlimefunItemStack("MO_TRANSMUTATION_WAND", Material.BLAZE_ROD, "&d蜕变魔杖", "", "&f用于转换方块/物品", "");
     //Misc
-    public static final SlimefunItemStack MO_GLASS_CUTTER = new SlimefunItemStack("MO_GLASS_CUTTER", Material.STONECUTTER, "&f玻璃刀", "", "&f更有效地将玻璃切割成窗格", "");
+    public static final SlimefunItemStack MO_GLASS_CUTTER = new SlimefunItemStack("MO_GLASS_CUTTER", Material.STONECUTTER, "&f玻璃刀", "", "&f将玻璃切割成玻璃板", "");
     //Info
 }
